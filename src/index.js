d48b7e0ca9ebc53233eb2cf4eeaed6dc13667853
pasharobin <pasharobin@tut.by>
2021-03-07 18:52:32 +0300

    feat: add functions max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (!array || (array.length == 0)) {
+    return 0;
+  }
+
+  array.sort((a, b) => a - b)
+
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if (!array || (array.length == 0)) {
+    return 0;
+  }
+
+  array.sort((a, b) => b - a)
+
+  return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if (!array || (array.length == 0)) {
+    return 0;
+  }
+
+  let sum = 0;
+  array.forEach(e => sum += e)
+
+  return sum/array.length
 }
